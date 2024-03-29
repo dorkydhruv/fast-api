0996705cb80ea1aa48010459568106472d55cc2a
dorkydhruv <thedhruvsharma@gmail.com>
2024-03-29 06:49:56 +0530

    pydantic

--- app/main.py
+++ app/main.py
@@ -1,41 +1,31 @@
-from typing import Optional
-import psycopg
+# import psycopg
 from psycopg.rows import dict_row
 from fastapi import Body, FastAPI, Response,status,HTTPException,Depends
-from pydantic import BaseModel
 from random import randint
 import time
 from . import models
 from sqlalchemy.orm import Session
 from .database import engine ,get_db
-
+from . import schemas
 app=FastAPI()
 models.Base.metadata.create_all(bind=engine)
-
-
-
-class Post(BaseModel):
-    title :str
-    content : str
-    published :bool = True
-
 
 
 @app.get("/")
 async def root():
     return {"message": "Wassup:)"}
 
-@app.get("/posts")
+@app.get("/posts",response_model=list[schemas.Post])
 def read_posts(db: Session=Depends(get_db)):
     # cursor.execute("""
     # SELECT * FROM posts
     # """)
     # posts = cursor.fetchall()
     posts = db.query(models.Post).all()
-    return {"data": posts}
+    return posts
 
-@app.post("/posts",status_code=status.HTTP_201_CREATED)
-def create_post(post: Post,db: Session=Depends(get_db)):
+@app.post("/posts",status_code=status.HTTP_201_CREATED,response_model=schemas.Post)
+def create_post(post: schemas.PostCreate,db: Session=Depends(get_db)):
     # cursor.execute(""" INSERT INTO posts (title,content,published) VALUES (%s,%s,%s) RETURNING *""",
     #                (post.title,post.content,post.published))
     # new_post = cursor.fetchone()
@@ -44,15 +34,15 @@ def create_post(post: Post,db: Session=Depends(get_db)):
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
-    return {"data":new_post}
+    return new_post
 
-@app.get("/posts/{id}")
+@app.get("/posts/{id}",response_model=schemas.Post)
 def get_post(id :int, response: Response,db: Session=Depends(get_db)):
     # cursor.execute("""SELECT * FROM posts WHERE id = %s""",(str(id),))
     # this_post = cursor.fetchone()
     this_post = db.query(models.Post).filter(models.Post.id == id).first()
     if this_post:
-        return {"data":this_post}
+        return this_post
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"post with id {id} not found")
 
 @app.delete("/posts/{id}",status_code=status.HTTP_204_NO_CONTENT)
@@ -66,8 +56,8 @@ def delete_post(id:int,db:Session=Depends(get_db)):
         return Response(status_code=status.HTTP_204_NO_CONTENT)
     raise HTTPException(status_code=404,detail=f"post with id {id} not found")
 
-@app.put("/posts/{id}")
-def update_post(id:int,post:Post,db:Session=Depends(get_db)):
+@app.put("/posts/{id}",response_model=schemas.Post)
+def update_post(id:int,post:schemas.PostCreate,db:Session=Depends(get_db)):
     # cursor.execute("""UPDATE posts SET title = %s, content=%s, published=%s WHERE id = %s RETURNING *""",(post.title,post.content,post.published,str(id),))
     # post= cursor.fetchone()
     post_query=db.query(models.Post).filter(models.Post.id == id)
@@ -75,5 +65,5 @@ def update_post(id:int,post:Post,db:Session=Depends(get_db)):
         # conn.commit()
         post_query.update(post.dict())
         db.commit()
-        return {"data":post_query.first()}
+        return post_query.first()
     raise HTTPException(status_code=404,detail=f"post with id {id} not found")
